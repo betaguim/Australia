e13dcc467c7974059fd009b0599643cfa9a86bcf
betaguim <103952411+betaguim@users.noreply.github.com>
2023-03-18 11:56:07 +0100

    Add files via upload

---
+++ BDAustralia.js
@@ -0,0 +1,146 @@
+var F = Array();         //fotografies
+
+F[0]="0.jpg";
+F[1]="1.jpg";
+F[2]="2.jpg";
+F[3]="3.jpg";
+F[4]="4.jpg";
+F[5]="5.jpg";
+F[6]="6.jpg";
+F[7]="7.jpg";
+F[8]="8.jpg";
+F[9]="9.jpg";
+F[10]="10.jpg";
+F[11]="11.jpg";
+F[12]="12.jpg";
+F[13]="13.jpg";
+F[14]="14.jpg";
+F[15]="15.jpg";
+F[16]="16.jpg";
+F[17]="17.jpg";
+F[17]="17.jpg";
+F[18]="18.jpg";
+F[19]="19.jpg";
+F[20]="20.jpg";
+F[21]="21.jpg";
+F[22]="22.jpg";
+F[23]="23.jpg";
+F[24]="24.jpg";
+F[25]="25.jpg";
+F[26]="26.jpg";
+F[27]="27.jpg";
+F[28]="28.jpg";
+F[29]="29.jpg";
+F[30]="30.jpg";
+F[31]="31.jpg";
+F[32]="32.jpg";
+F[33]="33.jpg";
+F[34]="34.jpg";
+F[35]="35.jpg";
+F[36]="36.jpg";
+F[37]="37.jpg";
+F[38]="38.jpg";
+F[39]="39.jpg";
+F[40]="40.jpg";
+F[41]="41.jpg";
+F[42]="42.jpg";
+F[43]="43.jpg";
+F[44]="44.jpg";
+F[45]="45.jpg";
+F[46]="46.jpg";
+F[50]="50.jpg";
+
+var C = Array(); 
+C[0]="Acacia aneura<br>És un arbust o petit arbre de l'interior àrid de Austràlia, de fusta molt dura, és utilitzat tan per a fer instruments com per fer foc, Inflorescències axil·lars amb flors de color groc brillant."
+
+C[01]="Acacia brachystachya<br>Arbust de fins a 5 m, amb tiges que es trenen i s'estenen. Presenta fil·lodis en lloc de fulles veritables. Floreix a l'octubre amb flors de color groc, agrupades en raïms cilíndrics d'uns 2 cm de longitud. Les beines són llargues i rectes."
+
+C[02]="Acacia cultriformis o acàcia de fulles de ganivet, s'utilitza com a arbre ornamental i per flor tallada, les flors són comestibles i també s'en extreu un colorant groc."
+
+C[03]="Acacia howittii<br>Arbust que es troba en àrees humides. És una espècie que s'usa com a planta ornamental."
+
+C[04]="Acacia longifolia<br>Les fulles són fil·lodis de forma linear o lanceolats amb 2 o 3 nervis longitudinals, les flors són grogues disposades en espigues axil·lars, floreix de març a maig. Acàcia, del grec akis = punta, fent referència a les espines de les acàcies africanes, les australianes no en solen tenir. Longifolia, del llatí longus i folia, vol dir fulles llargues."
+
+C[05]="Acacia truncata<br>"
+
+C[06]="Alyxia ruscifolia<br>És un arbust de zones plujoses australianes de fulles verticil·lades en grups de 4-5, acabades en espina a l'àpex, flors blanques oloroses amb 5 pètals i 5 sèpals. Els fruits presenten de 2 a 3 articles globosos d'un color taronja vermellós units en collar.<br>El nom del gènere podria venir del grec -una cadena-, referint-se als fruits aparellats, en forma de collar.<br> ruscifolia: fa referència a les fulles que recorden el gènere Ruscus."
+
+C[07]="Anigozanthus flavidus<br>Planta herbàcia perenne. Les fulles són basals, molt llargues amb poques fulles petites a la tija. Les flors són de forma tubular de color verd groguenc amb els estams de color taronja, densament cobertes de pilositat que sovint també cobreix les tiges."
+
+C[08]="Banksia integrifolia<br>Arbre, sovint d'aspecte arbustiu, les fulles són enteres en verticils de 3-5, són de color verd fort per sobre i de color plantejat per sota, les inflorescències són terminals formades per una espiga cilíndrica amb nombroses flors de color groc pàl·lid, les llavors són alades per facilitar la disseminació pel vent."
+
+C[09]="Banksia praemorsa<br>Les fulles són perennes i amb el marge serrat. Les seves flors són petites i agrupades en raïms densos que s’obren a la primavera i que donen lloc a un fruit resistent al foc. el fruit pot sobreviure el foc; aquestes altes temperatures provoquen l’obertura dels fol·licles i amb l’ajut del vent, les llavors alades són dispersades. Praemorsa del llatí, praemorsus, (arrencat d'una mossegada)"
+
+C[10]="Callistemon citrinus<br>Arbust de fulla perenne. els estams són de color vermell, porpra o lila amb les anteres de color fosc. Els ocells que cerquen el nèctar de les flors utilitzen aquesta espècie com a font d'aliment. L'epítet citrinus del llatí que vol dir cítric, pel seu aroma de llimona."
+
+C[11]="Callistemon pallidus<br>Els callistemons poden ser arbres o arbustos de fulles perennes, alternes i enteres. Però el més espectacular són les seves inflorescències, conjunt de flors agrupades en forma d'espiga gairebé a l'extrem de la tija. El que fa més vistosa tota la inflorescència no són els pètals, sinó els estams lliures molt més llargs que els pètals, de color vermell molt vistós, o a vegades blanc, verd o blanc. Es poden confondre amb les espècies del gènere Melaleuca, que també forma inflorescències d'aquesta mena, per diferenciar-les cal observar els estams amb la lupa. S'obté un compost químic -leptosmermona-, component d'un herbicida anomenat -Calisto-."
+
+C[12]="Callistemon violaceus<br>La inflorescència està formada per nombroses flors disposades alternadament en un eix principal i que formen un espiga pseudoterminal, que recorda un raspall de netejar tubs. Un cop finalitzada la floració, aquest eix principal continua creixent i forma una nova branca de fulles. Cada flor està formada per cinc sèpals i cinc pètals, però tots dos són minúsculs i no destaquen del conjunt. L’atractiu de la flor ve donat per la part masculina (l’androceu) que està format per nombrosos estams lliures de color vermell i entremig apareix el pistil que correspon a la part femenina."
+
+C[13]="Casuarina cristata<br>Arbre dioic amb la capçada coberta de branquetes verdes i primes amb fulles reduïdes a 6 dents molt petites que apareixen en els nusos de les branquetes."
+
+C[14]="Corymbia ficifolia<br>Arbre cultivat com ornamental per les seves flors vermelles i també com arbre d'alineació en carrers, no tolera gelades fortes ni massa humitat, abans era conegut com Eucalyptus ficifolia. Inflorescència terminal en corimbe formada per la unió d'umbel·les de 3 a 7 flors. ficifolia, perquè la forma de les fulles recorda les del ficus."
+
+C[15]="Corymbia torelliana<br>"
+
+C[16]="Dichelostemma ida-maia<br>Planta perenne que presenta un escap sense fulles que remata en una umbel·la que porta de 6 a 20 flors. Cada flor és un tub cilíndric de color vermell de dos a tres centímetres de llarg. L'extrem de cada tèpal s'arrissa cap enrere per revelar una superfície inferior blanca brillant. Els rínxols voregen la boca de la flor tubular formant una corona. És originaria del nord de Califòrnia i del sud d'Oregon als Estats Units, on creix a boscos de muntanya i prats costaners. També es cultiva com a planta ornamental per les seves flors vistoses de color vermell i crema."
+
+C[17]="Eremophila divaricata<br>"
+
+C[18]="Eremophila maculata<br>Arbust ornamental amb fulles estretes i flors tubulars vermelles, les fulles són tòxiques pel bestiar. Eremophila del grec, eremos, desert i phileo, amor, en referència a l'hàbitat de l'espècie. maculata tacat, per les marques de taques dins de la flor."
+
+C[19]="Eucalyptus caesia<br>Endemisme que amb poca quantitat d'exemplars, es troba sobre terrenys granítics. Les branques tendeixen a penjar fins al terra, presenta grans flors de color rosa-vermellós. Són molt cultivats com a planta ornamental, però s'han tornat rars a la natura. Té una pols blanca que cobreix les branques, els capolls de les flors i els fruits, d'aquí l'epítet llatí caesia que vol dir de color blau-grisenc."
+
+C[20]="Eucalyptus calycogona<br>Petit arbre de fins a 5m. d'alt, d'escorça llisa, sovint amb forma de mallee, resistent a les gelades, floreix a l'hivern o a la primavera, les flors són de color blanc cremós o rosa."
+
+C[21]="Eucalyptus cosmophylla<br>Arbust o petit arbre de forma arrodonida, flors de color crema unides directament a la tija. Creix en condicions pantanoses humides però tolera períodes en què el sòl està sec. És important recordar que tots Eucaliptus són sensibles a alts nivells de fòsfor."
+
+C[22]="Eucalyptus eritrocoris<br>La flor és molt espectacular, quan és tancada presenta un opercle caduc de color vermell i en obrir-se presenta vistosos estams de color blanc."
+
+C[23]="Eucalyptus kruseana<br>Arbust ornamental molt ramificat de fulles arrodonides amb la vora vermella, floreix a l'hivern amb flors de color crema."
+
+C[24]="Eucalyptus torquata<br>El poden distingir per l'opercle acabat en punta. Les flors, de color vermell o rosa, són grans 3,5 cm de diàmetre. Es fa servir com arbre d'alineació i com a planta ornamental, en un test gran. La llavor germina fàcilment i en dos anys ja pot florir."
+
+C[25]="Ficus australis<br>Coneguda també com a figuera de fulla petita. Aquesta espècie presenta un tret molt característic dels Ficus: l’emissió d’arrels aèries des de les branques i el tronc que creixen cap avall cercant la terra, conegudes amb el nom d’arrels fúlcries. Una vegada contacten amb el sòl arrelen immediatament i es transformen en columnes molt gruixudes que aporten estabilitat, nous nutrients i aigua a l’arbre, el qual arriba a assolir una grandària gegantina. Les flors són pol·linitzades per l’espècie de vespa Pleistodontes imperialis i mantenen un mutualisme obligat característic del gènere Ficus, on cada espècie té una vespa pol·linitzadora específica. Cal destacar que aquesta espècie no és pròpia de la zona mediterrània, sinó d’una gran franja marítima de clima tropical de la costa est d’Austràlia."
+
+C[26]="Grevillea crithmifolia<br>"
+
+C[27]="Grevillea cv. Golden<br>Arbres i arbusts de fulla perenne amb fulles alternes, sovint dentades o lobulades. Flors en raïms o panícules terminals, de vegades axil·lars, les flors estan disposades en parelles. El periant (calze i corol·la) és tubular. Androceu amb 4 estams. L’estil acostuma a ser llarg. Flors tubulars de forma corbada amb 4 estams i un estil llarg. El fruit en fol·licle llenyós, amb llavors alades."
+
+C[28]="Grevillea johnsonii<br>Arbust o petit arbre, molt ramificat, de creixement ràpid, fulles pinnatisectes (fulles amb divisions que arriben fins el nervi principal), més o menys sèssils (sense pecíol). Inflorescències en raïms terminals o axil·lars erectes de color rosat o vermell, el periant (Conjunt de peces estèrils que sovint envolta els elements fèrtils de la flor -calze i corol·la-) està fortament recorbat aproximadament des de la meitat, l'estil, també corbat és vermellós."
+
+C[29]="Grevillea olivacea<br>Arbust de fullatge dens, ornamental i flors de color albercoc a taronja que apareixen a l'hivern i primavera, és de creixement ràpid. Atreu els colibrís que pol·linitzen les flors. Olivacia pel color de fulla d'olivera."
+
+C[30]="Grevillea robusta<br>Arbre de fulla perenne i creixement ràpid, l'espècie més gran del gènere Grevillea, les fulles són compostes, bipinnades i recorden les frondes de les falgueres. floreix a la primavera, flors de color groc o taronja. (usos: planta d'interior, fusta per instruments musicals)"
+
+C[31]="Hakea bucculenta<br>Arbust o petit arbre de fulles llargues i estretes i flors vistoses de color vermell ataronjat que floreixen a l'hivern i primavera."
+
+C[32]="Hakea cristata<br>Arbust molt ramificat, pot fer d'1 a 2,5 m d'alt, fulles alternes amb el marge espinós o sencer, inflorescència axil·lar de flors blanques. El nom de Hakea bé del Baró Christian Ludwig von Hake, segle XVIII, botànic alemany. Cristata fa referència a la cresta que presenta el fruit d'aquesta espècie."
+
+C[33]="Hakea laurina<br>Es tracta de grans arbustos o petits arbres perennifolis que poden arribar als 6 m d'altura. Les fulles són alternes, coriàcies, solcades de nervis de color crema en el revers. Floreix durant la tardor i fins ben entrat l'hivern. Les inflorescències recorden un aguller, produeixen nèctar i un suau olor. Les flors semblen uns coxininets, d'aquí els ve el nom comú. El més visible són els estam d'un color groc o crema cap a rosat a mesura que envelleixen. El fruit és llenyós, dur i resta a les branques.<br>Hakea nom en honor del Baró Christian Ludwig von Hake, segle XVIII. L'epítet laurina bé del llatí laurus, per la semblança amb les fulles de llorer."
+
+C[34]="Hakea orthorrhyncha<br>Arbust de flors vermelles brillants, floreix a finals d'hivern i començament de primavera en raïms sobre les tiges velles, el fruit és una beina llenyosa punxeguda que conté dues llavors alades, la forma del fruit dona el nom de l'espècie, orthorrhyncha que deriva de les paraules gregues orto (recte) i rhynchos (bec) fent al·lusió a la forma recte de les càpsules de les llavors."
+
+C[35]="Hakea recurva<br>Arbust de fulles alternes lineals i cilíndriques. Forma inflorescències axil·lars racemoses de 20 a 40 flors, les flors de color blanc a groc. Recurva del llatí recurvus - corbat cap enrere, en referència a la forma de les fulles."
+
+C[36]="Lagunaria patersonii<br>=Arbre de ràpid creixement de port piramidal de jove, les flors són de color rosa malva, floreix al començament de l'estiu. És un arbre força resistent, excepte al fred, fins i tot a la salinitat del mar, permet la poda en formes geomètriques o piramidals. <br>Lagunaria pel botànic espanyol Andrés de Laguna (sigle XV-XVI). Patersonii, en honor del botánic i explorador escocés W. Paterson, que es creu va ser el primer en portar llavors a Anglaterra."
+
+C[37]="Melaleuca nesophilla<br>Les flors apareixen agrupades al final de les branques, i formen una inflorescència globular, són de color porpra amb les anteres grogues. És una de les espècies més utilitzades en jardineria, es poc freqüent a la natura i es considera que hi ha més exemplars en cultiu que en estat natural."
+
+C[38]="Melaleuca styphelioides<br>"
+
+C[39]="Myoporum<br>Es conexen 30 espècies del gènere, divuit de les quals són endèmiques d'Austràlia.<br>Són arbres o arbustos de fulla perenne, les fulles poden ser alternes senceres, dentades o serrades. Les flors, solitàries o en cims axil·lars, tenen el calze amb 5 sèpals lliures o soldats i la corol·la acampanada o lleugerament bilabiada,  5 pètals més o menys patents. Hi ha generalment 4 estams -ocasionalment 5, fins i tot en la mateixa espècie- amb anteres de 2 teques. L'ovari té 2-4 lòculs, i el fruit és generalment carnós amb llavors ovoides."
+
+C[40]="Pimelea ferrruginea<br>Arbust baix que forma una mata arrodonida i compacte que no acostuma a superar el metre d'altura. Les tiges neixen des de la base de manera regular i estan cobertes de petites fulles brillants, disposades en dos parells de files oposades, floració fragant i vistosa en raïms de petitíssims flors constituïdes per quatre pètals de color rosa brillant i llargs filaments. Suporta bé els ambients salins i sorrencs que s'assemblen al seu propi hàbitat natural les zones sorrenques de la costa australiana"
+
+C[41]="Senna artemisoides<br>Arbust d'uns 3 m d'alçada de fulles pinnades de 1 a 8 parells de folíols. Produeix moltes flors grogues a l'hivern i a la primavera, flors d'uns 1,5 cm de diàmetre, seguides de beines verdes planes."
+
+C[42]="Stenocarpus sinuatus<br>"
+
+C[43]="Thrytomene saxicola<br>"
+
+C[44]="Westringia fruticosa<br>Arbust dens i arrodonit de fulla perenne i floració abundant, gairebé tot l'any, flors blanques. És resistent als ambients salins propers al mar. El nom en record de Johan Peter Westring (1753-1833), botànic i metge del rei Carles XIII de Suècia, que va ser alumne de Linné."
+
+C[45]="Xanthorrhoea glauca<br>Plantes de creixement lent, 1-2 cm anuals, poden viure uns 600 anys El tronc triga 10 anys a fer-se i està format per la base de les fulles velles les quals queden entrelligades per una reïna natural. Al cap d'uns 20 anys apareix un grup de fulles linears sobre del tronc que fa l'efecte d'una mata d'herba sobre el tronc. La floració és primaveral, però, no anual normalment després d'un foc, forma una espiga de més de 2 metres amb flors petites de color blanc cremós, les flors contenen nèctar, utilitzat pels aborígens per a fer begudes dolces."
+
+C[50]="Austràlia"
